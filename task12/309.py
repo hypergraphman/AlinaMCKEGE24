63c142c560f93f97b4a0f04dbd1f50d8892ecebb
Руслан Аитов <r_aitov@mail.ru>
2023-02-07 20:38:50 +0300

    task 12

---
+++ task12/309.py
@@ -0,0 +1,11 @@
+s = '3' * 70
+while '333' in s or '77777' in s:
+    if '333' in s:
+        s = s.replace('333', '77', 1)
+    else:
+        s = s.replace('77777', '7', 1)
+summa = 0
+for el in s:
+    summa += int(el)
+print(summa)
+print(sum(map(int, s)))
